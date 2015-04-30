faa9b290a04553b00467a15a43b00636d9f61ceb
James <james.l.davis@outlook.com>
2015-04-30 15:19:46 -0400

    Updated References for MVC application

--- clrprofiler/clrprofiler_i.h
+++ clrprofiler/clrprofiler_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Thu Apr 30 10:48:45 2015
+/* at Thu Apr 30 10:56:50 2015
  */
 /* Compiler settings for clrprofiler.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
